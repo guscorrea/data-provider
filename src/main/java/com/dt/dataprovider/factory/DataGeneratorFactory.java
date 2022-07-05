cd0db962d57916fed57991cb5d94f0639bfbaa68
Gustavo Correa <gustavo.correa@sap.com>
2022-07-04 23:18:26 -0300

    enabled data generation for ANM

--- src/main/java/com/dt/dataprovider/factory/DataGeneratorFactory.java
+++ src/main/java/com/dt/dataprovider/factory/DataGeneratorFactory.java
@@ -4,22 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.dt.dataprovider.model.enums.ComponentType;
+import com.dt.dataprovider.service.AnmDataService;
 import com.dt.dataprovider.service.ChokeValveDataService;
 import com.dt.dataprovider.service.DataGeneratorService;
 
 @Component
 public class DataGeneratorFactory {
 
-	private ChokeValveDataService chokeValveDataService;
+	private final ChokeValveDataService chokeValveDataService;
+
+	private final AnmDataService anmDataService;
 
 	@Autowired
-	public DataGeneratorFactory(ChokeValveDataService chokeValveDataService) {
+	public DataGeneratorFactory(ChokeValveDataService chokeValveDataService, AnmDataService anmDataService) {
 		this.chokeValveDataService = chokeValveDataService;
+		this.anmDataService = anmDataService;
 	}
 
 	public DataGeneratorService getDataGeneratorService(ComponentType componentType) {
 		if (ComponentType.choke.equals(componentType)) {
 			return chokeValveDataService;
+		}
+		else if (ComponentType.anm.equals(componentType)) {
+			return anmDataService;
 		}
 		return null;
 	}
